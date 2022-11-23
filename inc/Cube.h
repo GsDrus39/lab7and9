b964fed8a9aaae1e37aba9e61424a2dd7a0719ff
GsDrus39 <63257358+GsDrus39@users.noreply.github.com>
2022-11-23 14:36:13 +0200

    Update Cube.h
    Поправил названия в private

--- inc/Cube.h
+++ inc/Cube.h
@@ -3,16 +3,16 @@
 class Cube
 {
 private:
-    double pr_side_len;
+    double m_side_len;
 public:
     Cube(double side_len)
     {
-        pr_side_len = side_len;
+        m_side_len = side_len;
     }
 
     double get_diagonal()
     {
-        double a = pow((pr_side_len * pr_side_len * 2), 0.5);
-        return pow((a * a + pr_side_len * pr_side_len), 0.5);
+        double a = pow((m_side_len * m_side_len * 2), 0.5);
+        return pow((a * a + m_side_len * m_side_len), 0.5);
     }
 };
