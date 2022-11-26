8be4b62cc1d0384de0ce65749bef073495c1149c
GsDrus39 <63257358+GsDrus39@users.noreply.github.com>
2022-11-26 10:37:01 +0200

    Create Cube.cpp

---
+++ inc/Cube.cpp
@@ -0,0 +1,10 @@
+#include <cmath>
+#include "Cube.h"
+using namespace Cubens;
+Cube(side_len){
+    m_side_len = side_len;
+}
+double Cube::get_diagonal(){
+    double a = pow((m_side_len * m_side_len * 2), 0.5);
+    return pow((a * a + m_side_len * m_side_len), 0.5);
+}
